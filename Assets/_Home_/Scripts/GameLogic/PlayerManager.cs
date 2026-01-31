2755d17bef376ccefad8db32e00c9901f4004e32
Dani <danithegamedev@gmail.com>
2026-01-31 17:14:47 +0100

    cambios mergeados

--- Assets/_Home_/Scripts/GameLogic/PlayerManager.cs
+++ Assets/_Home_/Scripts/GameLogic/PlayerManager.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int playerIdx;
+    private List<int> targetsInRange;
+    private GameManager gameManager;
+    
+    private void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Friend"))
+        {
+            targetsInRange.Add(other.gameObject.GetComponent<Target>().getIndex());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {if (other.CompareTag("Friend"))
+        {
+            targetsInRange.Remove(other.gameObject.GetComponent<Target>().getIndex());
+        }
+    }
+
+    public void InteractWithTargets()
+    {
+        foreach (int idx in targetsInRange)
+        {
+            gameManager.InteractWithTarget(idx, playerIdx);
+        }
     }
 }
