8d357d400d3eae15c740bd1aef58fcec094e49e5
SerghG <sergi.rubsal.97@gmail.com>
2026-02-01 12:33:45 +0100

    Añadidas las funciones de audio

---
+++ Assets/_Home_/Scripts/SoundController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    [SerializeField] List<AudioClip> sfxList;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void reproduceSfxClip(int numAudio)
+    {
+        if(!audioSource.isPlaying)
+            audioSource.PlayOneShot(sfxList[numAudio]);
+    }
+}
