72cf86e086055e461f9cb7918adbeab4645d55cd
Javi <mrvizious@gmail.com>
2026-02-01 12:34:55 +0100

    hotfix: Catch mechanic works with more than one player

--- Assets/_Home_/Scripts/GameLogic/PlayerManager.cs
+++ Assets/_Home_/Scripts/GameLogic/PlayerManager.cs
@@ -6,20 +6,12 @@ using Sirenix.OdinInspector;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private int playerIdx;
-    private HashSet<int> targetsInRange = new HashSet<int>();
+    public HashSet<int> targetsInRange = new HashSet<int>();
     private GameManager gameManager;
 
     private void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
-    }
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.CompareTag("Friend"))
-        {
-            AddEnemyToEnemiesInRange(other.gameObject.GetComponent<Target>().getIndex());
-        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -39,23 +31,9 @@ public class PlayerManager : MonoBehaviour
     {
         if (other.CompareTag("Friend"))
         {
-            targetsInRange.Remove(other.gameObject.GetComponent<Target>().getIndex());
+            int friendIndex = other.gameObject.GetComponent<Target>().getIndex();
+            targetsInRange.Remove(friendIndex);
         }
     }
 
-    public void Interact(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            InteractWithTargets();
-        }
-    }
-    public void InteractWithTargets()
-    {
-        if (!gameManager.TryCatch()) return;
-        foreach (int idx in targetsInRange)
-        {
-            gameManager.InteractWithTarget(idx, playerIdx);
-        }
-    }
 }
